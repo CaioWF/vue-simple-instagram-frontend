22a17a64bc61029fdb7fa758ef377409d3ee1075
CaioWF <caioweliton@alu.ufc.br>
2020-02-08 11:05:58 -0300

    add new post page & styles

--- src/pages/new/New.vue
+++ src/pages/new/New.vue
@@ -1,9 +1,100 @@
 <template>
-  <div>New</div>
+  <form id="new-post" @submit.prevent="submit">
+    <input type="file" @change="onImageChange" required/>
+    <input
+      type="text"
+      v-model="form.author"
+      name="author"
+      placeholder="Autor do post"
+      required
+    />
+    <input
+      type="text"
+      v-model="form.description"
+      name="description"
+      placeholder="Descrição do post"
+      required
+    />
+    <input
+      type="text"
+      v-model="form.place"
+      name="place"
+      placeholder="Local do post"
+      required
+    />
+    <input
+      type="text"
+      v-model="form.hashtags"
+      name="hashtags"
+      placeholder="Hashtags do post"
+      required
+    />
+    <button type="submit">Enviar</button>
+  </form>
 </template>
 
 <script>
-export default {
+import { createNamespacedHelpers } from 'vuex';
 
+const { mapState, mapActions } = createNamespacedHelpers('posts');
+
+export default {
+  data: () => ({
+    file: {},
+  }),
+  computed: {
+    ...mapState({ form: 'postForm' }),
+  },
+  methods: {
+    ...mapActions(['addPost', 'addImageOnPost']),
+    onImageChange(event) {
+      this.addImageOnPost(event.target.files[0]);
+    },
+    submit() {
+      this.addPost();
+      this.$router.push({ name: 'feed' });
+    },
+  },
 };
 </script>
+
+<style lang="stylus" scoped>
+form#new-post
+  max-width 580px
+  margin 30px auto 0
+  padding 30px
+  background #fff
+  border 1px solid #ddd
+
+  display flex
+  flex-direction column
+
+  input
+    margin-bottom 10px
+    outline 0
+    transition all 0.2s ease
+    &:focus
+      box-shadow 0 0 2px #7159c1
+
+  input[type='text']
+    height 38px
+    border-radius: 4px
+    border 1px solid #ddd
+    padding 0 20px
+    font-size 14px
+
+  button
+    padding 10px 20px
+    border-radius 4px
+    border 0
+    background #7159c1
+    color #FFF
+    font-size 14px
+    font-weight bold
+    cursor pointer
+    outline 0
+    transition all 0.2s ease
+
+    &:hover
+      opacity 0.8
+</style>
--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -2,6 +2,13 @@ import api from '@/services/api';
 
 const state = {
   posts: [],
+  postForm: {
+    image: null,
+    author: '',
+    place: '',
+    description: '',
+    hashtags: '',
+  },
 };
 
 const getters = {
@@ -10,8 +17,34 @@ const getters = {
 
 const actions = {
   async fetchAllPosts({ commit }) {
-    const response = await api.get('posts');
-    commit('setPosts', response.data);
+    try {
+      const response = await api.get('posts');
+      commit('setPosts', response.data);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err.message);
+    }
+  },
+  // eslint-disable-next-line no-shadow
+  async addPost({ state, commit }) {
+    try {
+      const data = new FormData();
+      data.append('image', state.postForm.image);
+      data.append('author', state.postForm.author);
+      data.append('place', state.postForm.place);
+      data.append('description', state.postForm.description);
+      data.append('hashtags', state.postForm.hashtags);
+
+      await api.post('posts', data);
+
+      commit('cleanForm');
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err.message);
+    }
+  },
+  addImageOnPost({ commit }, payload) {
+    commit('setImageOnPost', payload);
   },
 };
 
@@ -19,6 +52,18 @@ const mutations = {
   // eslint-disable-next-line no-shadow
   setPosts(state, payload) {
     state.posts = payload;
+  },
+  // eslint-disable-next-line no-shadow
+  setImageOnPost(state, payload) {
+    state.postForm.image = payload;
+  },
+  // eslint-disable-next-line no-shadow
+  cleanForm(state) {
+    state.postForm.image = null;
+    state.postForm.author = '';
+    state.postForm.place = '';
+    state.postForm.description = '';
+    state.postForm.hashtags = '';
   },
 };
 
